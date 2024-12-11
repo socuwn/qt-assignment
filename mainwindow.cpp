03d0d3f09364196c4a76bb48f5ae3f7380227f08
socuwn <32123551+socuwn@users.noreply.github.com>
2024-12-11 22:06:05 +0100

    Instructions, log, reference table, and improvements
    added usage instructions
    added log history for all whole inputs
    added reference table mapping characters to morse code
    improved code to text function
    cleaned up loose ends

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,14 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QMessageBox>
+
 
 QString buffer = "";
-bool mode = true;
+
+const int MODE_CHAR = 1;
+const int MODE_CODE = 2;
+int mode = MODE_CHAR;
 
 
 std::unordered_map<char, QString> alphabet_map_char = {
@@ -74,96 +79,24 @@ std::unordered_map<char, QString> alphabet_map_char = {
 std::unordered_map<QString, char> alphabet_map_code;
 
 
-
-
-QString letter_to_morse(char letter)
-{
-    if ('A' <= letter && letter <= 'Z') {
-        letter += 'a' - 'A';
-    }
-
-    switch (letter) {
-    case ' ' : return "   ";
-    case '!' : return "-.-.--";
-    case '\"': return ".-..-.";
-
-    case '$' : return "...-..-";
-
-    case '&' : return ".-...";
-    case '\'': return ".----.";
-    case '(' : return "-.--.";
-    case ')' : return "-.--.-";
-
-    case '+': return ".-.-.";
-    case ',': return "--..--";
-    case '-': return "-....-";
-    case '.': return ".-.-.-";
-    case '/': return "-..-.";
-    case '0': return "-----";
-    case '1': return ".----";
-    case '2': return "..---";
-    case '3': return "...--";
-    case '4': return "....-";
-    case '5': return ".....";
-    case '6': return "-....";
-    case '7': return "--...";
-    case '8': return "---..";
-    case '9': return "----.";
-    case ':' : return "---...";
-    case ';' : return "-.-.-.";
-
-    case '=' : return "-...-";
-
-    case '?': return "..--..";
-    case '@': return ".--.-.";
-
-    case '_': return "..--.-";
-
-    case 'a': return ".-";
-    case 'b': return "-...";
-    case 'c': return "-.-.";
-    case 'd': return "-..";
-    case 'e': return ".";
-    case 'f': return "..-.";
-    case 'g': return "--.";
-    case 'h': return "....";
-    case 'i': return "..";
-    case 'j': return ".---";
-    case 'k': return "-.-";
-    case 'l': return ".-..";
-    case 'm': return "--";
-    case 'n': return "-.";
-    case 'o': return "---";
-    case 'p': return ".--.";
-    case 'q': return "--.-";
-    case 'r': return ".-.";
-    case 's': return "...";
-    case 't': return "-";
-    case 'u': return "..-";
-    case 'v': return "...-";
-    case 'w': return ".--";
-    case 'x': return "-..-";
-    case 'y': return "-.--";
-    case 'z': return "--..";
-
-    default: return "";
-    }
-}
-
-
 QString text_to_morse(QString text)
 {
     QString r;
+
     for (auto c : text) {
+
         char letter = c.toLatin1();
+
         if ('A' <= letter && letter <= 'Z') {
             letter += 'a' - 'A';
         }
+
         if (alphabet_map_char.find(letter) != alphabet_map_char.end()) {
           QString mark = alphabet_map_char[letter];
           r.push_back(mark + " ");
         }
     }
+
     return r;
 }
 
@@ -172,30 +105,20 @@ QString morse_to_text(QString text)
 {
     QString r;
 
-    QString code = "";
+    QStringList codes = text.split(' ', Qt::SkipEmptyParts);
 
-    for (auto c : text) {
-        if (c == ' ') {
-            if (alphabet_map_code.find(code) != alphabet_map_code.end()) {
-                char letter = alphabet_map_code[code];
-                r.push_back(letter);
-            }
-            code = "";
-        } else {
-            code.push_back(c);
-            if (alphabet_map_code.find(code) != alphabet_map_code.end()) {
-                char letter = alphabet_map_code[code];
-                qInfo() << "code recognized: " << code << " => " << letter;
-            } else {
-                qWarning() << "code not recognized: " << c;
-            }
+    for (auto code : codes) {
+        if (alphabet_map_code.find(code) != alphabet_map_code.end()) {
+            char letter = alphabet_map_code[code];
+            r.push_back(letter);
         }
     }
+
     return r;
 }
 
 
-bool key_valid(int k)
+bool key_valid_character(int k)
 {
     return (0x20 <= k && k <= 0x22)
         || (0x26 <= k && k <= 0x29)
@@ -211,29 +134,83 @@ bool key_valid(int k)
 }
 
 
+bool key_valid_code(int k)
+{
+    return k == 0x20
+        || k == 0x2d
+        || k == 0x2e
+    ;
+}
+
 void MainWindow::keyPressEvent(QKeyEvent * event)
 {
-    //qDebug() << "key pressed: " << event->text() << "\n";
 
     if (event->key() == Qt::Key_Tab) {
-        mode = !mode;
-        buffer = "";
+
+        if (mode == MODE_CHAR) {
+            buffer = text_to_morse(buffer);
+            mode = MODE_CODE;
+            ui->label_character_input->setStyleSheet("background: gray");
+            ui->label_code_input->setStyleSheet("background: white");
+        } else if (mode == MODE_CODE) {
+            buffer = morse_to_text(buffer);
+            mode = MODE_CHAR;
+            ui->label_character_input->setStyleSheet("background: white");
+            ui->label_code_input->setStyleSheet("background: gray");
+        }
+
+        return;
+
     } else if (event->key() == Qt::Key_Backspace) {
+
         buffer.erase(buffer.end() - 1);
-    } else if (key_valid(event->key())) {
-        buffer.push_back(event->text());
-    }
 
-    if (buffer.size() > 32) {
-        buffer.remove(0, buffer.size() - 32);
-    }
+    } else if (mode == MODE_CHAR && key_valid_character(event->key())) {
 
-    if (mode) {
-        ui->label_text_english->setText(buffer);
-        ui->label_text_morse->setText(text_to_morse(buffer));
+        if (buffer.size() > 255) {
+            QMessageBox mbox;
+            mbox.critical(0, "ERROR", "Input buffer size is limited to 256 characters");
+            return;
+        }
+
+        if (event->text() == ' ') {
+            ui->list_log_words->addItem(buffer);
+            ui->list_log_codes->addItem(text_to_morse(buffer));
+            buffer = "";
+
+        } else {
+            buffer.push_back(event->text());
+        }
+
+
+    } else if (mode == MODE_CODE && key_valid_code(event->key())) {
+
+        if (buffer.size() > 255) {
+            QMessageBox mbox;
+            mbox.critical(0, "ERROR", "Input buffer size is limited to 256 characters");
+            return;
+        }
+
+        if (buffer.endsWith(' ') && event->text() == ' ') {
+            ui->list_log_words->addItem(morse_to_text(buffer));
+            ui->list_log_codes->addItem(buffer);
+            buffer = "";
+
+        } else {
+            buffer.push_back(event->text());
+        }
+
     } else {
-        ui->label_text_morse->setText(buffer);
-        ui->label_text_english->setText(morse_to_text(buffer));
+        return;
+    }
+
+    if (mode == MODE_CHAR) {
+        ui->label_character_input->setText(buffer);
+        ui->label_code_input->setText(text_to_morse(buffer));
+
+    } else if (mode == MODE_CODE) {
+        ui->label_code_input->setText(buffer);
+        ui->label_character_input->setText(morse_to_text(buffer));
     }
 }
 
@@ -244,14 +221,32 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    ui->table_character_code->setRowCount(alphabet_map_char.size());
+
+    int i = 0;
     for (auto v : alphabet_map_char) {
+
         alphabet_map_code[v.second] = v.first;
+
+        QTableWidgetItem * item_character = new QTableWidgetItem((QString)v.first);
+        QTableWidgetItem * item_code = new QTableWidgetItem(v.second);
+        QTableWidgetItem * item_ascii = new QTableWidgetItem(QString::number(v.first));
+        item_character->setFlags(item_character->flags() ^ Qt::ItemIsEditable);
+        item_code->setFlags(item_code->flags() ^ Qt::ItemIsEditable);
+        item_ascii->setFlags(item_ascii->flags() ^ Qt::ItemIsEditable);
+
+        ui->table_character_code->setItem(i, 0, item_character);
+        ui->table_character_code->setItem(i, 1, item_code);
+        ui->table_character_code->setItem(i, 2, item_ascii);
+
+        i++;
     }
 
-    buffer = "He is in the garb of a kwaker";
+    ui->label_character_input->setStyleSheet("background: white");
+    ui->label_code_input->setStyleSheet("background: gray");
 
-    ui->label_text_english->setText(buffer);
-    ui->label_text_morse->setText(text_to_morse(buffer));
+    ui->label_code_input->setText(text_to_morse(buffer));
+    ui->label_character_input->setText(buffer);
 }
 
 MainWindow::~MainWindow()
